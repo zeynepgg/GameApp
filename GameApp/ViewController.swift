4679dd129222f16f19ee59384342e372532d4fe2
zeynepgg <zeynepgursoy@outlook.com>
2022-03-05 21:35:07 +0300

    first commit

---
+++ GameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameApp
+//
+//  Created by Zeynep Gizem Gürsoy on 5.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
